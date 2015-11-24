2fd246f73bc1dd46294b0046b8e38601c1a47fc0
Merkurijus <matasjonys@gmail.com>
2015-11-24 15:34:24 +0700

    Init commit

---
+++ package.js
@@ -0,0 +1,37 @@
+Package.describe({
+  name: 'tapfuse:meteor-instagram-api',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  documentation: 'README.md'
+});
+
+var C = 'client';
+var S = 'server';
+var CS = [C, S];
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+  // Core
+  api.use([
+    'templating',
+    'ecmascript',
+    'http'
+    ]);
+  // 3rd party
+  // api.use([
+  //   ]);
+
+  api.addFiles('lib/tp-meteor-instagram-api-server.js', CS);
+
+  api.export('InstagramApi');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('tapfuse:meteor-instagram-api');
+  api.addFiles('tests/package-tests.js');
+});
